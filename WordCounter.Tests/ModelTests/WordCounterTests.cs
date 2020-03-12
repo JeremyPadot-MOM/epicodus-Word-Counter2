605786866f415eae909953ee3ad4afa4196d5dfc
Jeremy Kale Padot <jeremy@padot.us>
2020-03-11 17:57:01 -0700

    fail test, proving loop runs to the end of the userInput

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -73,7 +73,7 @@ namespace Counter.Tests
     [TestMethod]
     public void CheckInput_CheckUserInputIsWord_CheckInput()
     {
-      string userWord = "234";
+      string userWord = "cat1";
       string userSentence = "Today I'm taking my cat to the cathedral";
       UserInput newUserInput = new UserInput(userWord, userSentence);
       
