bd679e2a1f7eeebaceda71b41a5a2d8776f6ec58
Jeremy Kale Padot <jeremy@padot.us>
2020-03-11 17:57:50 -0700

    fail test. userInput mixed letters and numbers

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -73,7 +73,7 @@ namespace Counter.Tests
     [TestMethod]
     public void CheckInput_CheckUserInputIsWord_CheckInput()
     {
-      string userWord = "cat1";
+      string userWord = "c1a2t";
       string userSentence = "Today I'm taking my cat to the cathedral";
       UserInput newUserInput = new UserInput(userWord, userSentence);
       
